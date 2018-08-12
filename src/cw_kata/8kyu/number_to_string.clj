a8381541cbaef74e8fab4cfa69d26f5f3218b6ad
myungjaeyu <rootcode10@gmail.com>
2018-08-12 15:03:04 +0900

    [8kyu] Convert a Number to a String!

---
+++ src/cw_kata/8kyu/number_to_string.clj
@@ -0,0 +1,6 @@
+(ns cw-kata.8kyu.number-to-string)
+
+; 숫자를 문자열로 반환
+
+(defn number-to-string [num]
+  (str num))
---
+++ test/cw_kata/8kyu/number_to_string_test.clj
@@ -0,0 +1,15 @@
+(ns cw-kata.8kyu.number-to-string-test
+  (:use [cw-kata.8kyu [number-to-string]])
+  (:require [clojure.test :refer :all]))
+
+(deftest a-test1
+  (testing "Test1"
+    (is (= (number-to-string 0) "0"))))
+
+(deftest a-test2
+  (testing "Test2"
+    (is (= (number-to-string 50) "50"))))
+
+(deftest a-test3
+  (testing "Test3"
+    (is (= (number-to-string 144) "144"))))
