a95212859ef614d5590a2e0bbb41a87d72f1b95d
myungjaeyu <rootcode10@gmail.com>
2018-08-12 17:22:06 +0900

    [7kyu] Last

---
+++ src/cw_kata/7kyu/last_val.clj
@@ -0,0 +1,14 @@
+(ns cw-kata.7kyu.last-val)
+
+; 주어진 값의 마지막 인수를 반환
+
+; (defn last-val [lst]
+;   (first (reverse lst)))
+
+; (defn last-val [lst]
+;   (->> lst
+;        reverse
+;        first))
+
+(defn last-val [lst]
+  (last lst))
---
+++ test/cw_kata/7kyu/last_val_test.clj
@@ -0,0 +1,9 @@
+(ns cw-kata.7kyu.last-val-test
+  (:use [cw-kata.7kyu [last-val]])
+  (:require [clojure.test :refer :all]))
+
+(deftest last-val-test1
+  (is (= (last-val [1 9 13 1 99 9 9 13]) 13)))
+
+(deftest last-val-test2
+  (is (= (last-val "\"I suppose I should learn Lisp, but it seems so foreign.\" - Paul Graham, Nov 1983") \3)))
