2c2383dc19c056ab4df4b88da216fdb420cbb041
myungjaeyu <rootcode10@gmail.com>
2018-08-12 15:15:52 +0900

    [8kyu] String repeat

---
+++ src/cw_kata/8kyu/repeat_str.clj
@@ -0,0 +1,7 @@
+(ns cw-kata.8kyu.repeat-str)
+
+; 문자열을 주어진 수 만큼 반복하여 반환
+
+(defn repeat-str [n s]
+  (->> (repeat n s)
+       (apply str)))
---
+++ test/cw_kata/8kyu/repeat_str_test.clj
@@ -0,0 +1,15 @@
+(ns cw-kata.8kyu.repeat-str-test
+  (:use [cw-kata.8kyu [repeat-str]])
+  (:require [clojure.test :refer :all]))
+
+(deftest a-test1
+  (testing "Test1"
+    (is (= (repeat-str 4 "a") "aaaa"))))
+
+(deftest a-test2
+  (testing "Test2"
+    (is (= (repeat-str 3 "hello ") "hello hello hello "))))
+
+(deftest a-test3
+  (testing "Test3"
+    (is (repeat-str 2 "abc") "abcabc")))
