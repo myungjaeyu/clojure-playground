5538fe8f356f135c4cca6def94c3dd3accc9cdc6
myungjaeyu <rootcode10@gmail.com>
2018-08-12 14:12:52 +0900

    [7kyu] Find the vowels

---
+++ src/cw_kata/7kyu/vowel_indeices.clj
@@ -0,0 +1,16 @@
+(ns cw-kata.7kyu.vowel-indeices)
+
+; 모음의 인덱스를 리스트에 반환
+
+;  a e i o u y
+
+; https://clojuredocs.org/clojure.core/keep-indexed
+
+(def vowels #{\a \e \i \o \u \y})
+
+(defn vowel-indeices [s]
+  (->> s
+       clojure.string/lower-case
+       (keep-indexed #(when (vowels %2)
+                    (inc %1)))
+       vec))
---
+++ test/cw_kata/7kyu/vowel_indeices_test.clj
@@ -0,0 +1,15 @@
+(ns cw-kata.7kyu.vowel-indeices-test
+  (:use [cw-kata.7kyu [vowel-indeices]])
+  (:require [clojure.test :refer :all]))
+
+(deftest vowel-indeices-test1
+  (is (= (vowel-indeices "mmmm") [])))
+
+(deftest vowel-indeices-test2
+  (is (= (vowel-indeices "super") [2,4])))
+
+(deftest vowel-indeices-test3
+  (is (= (vowel-indeices "apple") [1,5])))
+
+(deftest vowel-indeices-test4
+  (is (= (vowel-indeices "yomama") [1,2,4,6])))
